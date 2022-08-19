1a5bb294f1146fcea12266ec134533172c06f0ce
sweetsoph <sophia.verardods@gmail.com>
2022-08-19 16:25:02 -0300

    início e cadastro terminadas, bugs solucionados
    Co-authored-by: Ryan Gualberto <RyanGualberto@users.noreply.github.com>

--- assets/styles/style.js
+++ assets/styles/style.js
@@ -1,9 +1,29 @@
-const btnMenu = document.getElementById('btnMenu');
+// const btnMenu = document.getElementById('btnMenu');
 
-function toggleMenu() {
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active');
+// function toggleMenu() {
+//     const nav = document.getElementById('nav');
+//     nav.classList.toggle('active');
+// }
+
+// btnMenu.addEventListener('click', toggleMenu());
+
+var formProfessores = document.querySelector('.group-professores')
+var formAlunos = document.querySelector('.group-alunos')
+var tipoUsuario = document.querySelector('#tipoUsuario');
+
+tipoUsuario.addEventListener("change",() => {
+    if(tipoUsuario.value == "professor"){
+        formProfessores.style.display = "flex";
+        formAlunos.style.display = "none";
+    }else if(tipoUsuario.value == 'aluno'){
+        formAlunos.style.display = 'flex';
+        formProfessores.style.display = "none";
+    } else {
+        formProfessores.style.display = "none";
+        formAlunos.style.display = "none";
+    }
+})
+
+function scrollTo(x, y){
+    window.scroll(x, y)
 }
-
-btnMenu.addEventListener('click', toggleMenu());
-
