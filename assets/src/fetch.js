0cbbeff4fda1e35f13bb27a85fee2f5772e85c22
demershow <demersontorres520@gmail.com>
2022-08-25 00:24:59 -0300

    add fetch

--- assets/src/fetch.js
+++ assets/src/fetch.js
@@ -13,3 +13,17 @@ function Login() {
     .then((Response) => Response.json())
     .then((json) => console.log(json.data));
 }
+
+let response = fetch('http://localhost:3333/api/admin/professores/')
+
+fetch('http://localhost:3333/api/admin/professores/')
+    .then(response => response.json())
+    .then((json) => {
+      let professores = (json.data)
+
+      console.log(professores)
+
+    }) .catch(err => console.error('Erro de solicitação', err));
+
+    
+
